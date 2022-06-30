3f293e4374e725d5f76c2b9edf55777c38ad1156
miaomiao <895489555@qq.com>
2022-06-30 20:51:16 +0200

    Add existing file

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,14 @@
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faFacebook,
+  faInstagram,
+   
+} from '@fortawesome/free-brands-svg-icons'
 
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
@@ -33,6 +41,47 @@ const Contact = () => {
             interested in a junior frontend developer offer. My core skills are
             React, Express and Mongo.
           </h2>
+
+        <ul className='contactInfo'>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/miaomiao-manes-9b91b6175/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/miaomiaomanes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/yangmiaomiao1223"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+          </a>
+        </li>
+       
+        <li>
+          <a
+            href="https://www.instagram.com/yang.miaomiao/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+     
+        </ul>
         
         </div>
         <div className="info-map">
